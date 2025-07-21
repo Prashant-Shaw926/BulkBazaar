1c7439ca051d7dfecd204b5f2c7af165293b6b6d
shashank-a21 <shashankanand705@gmail.com>
2025-07-22 00:23:54 +0530

    Added Boilerplate to AuthForm & RoleSelector Screen

---
+++ components/auth/RoleSelector.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const RoleSelector = () => {
+  return (
+    <View>
+      <Text>RoleSelector</Text>
+    </View>
+  )
+}
+
+export default RoleSelector
