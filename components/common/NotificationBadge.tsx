cdbf4e42c299784fdbb1501b4fb721938dc6dac1
shashank-a21 <shashankanand705@gmail.com>
2025-07-22 00:37:05 +0530

    Added Boilerplate to Components/common files

---
+++ components/common/NotificationBadge.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const NotificationBadge = () => {
+  return (
+    <View>
+      <Text>NotificationBadge</Text>
+    </View>
+  )
+}
+
+export default NotificationBadge
