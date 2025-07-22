ae99c09fa4d17a800dc81b03ad69ad0914d56f63
shashank-a21 <shashankanand705@gmail.com>
2025-07-22 11:33:21 +0530

    Added Boilerplate to components/customer,navigation,owner,ui files

---
+++ components/owner/OrderStatusUpdate.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const OrderStatusUpdate = () => {
+  return (
+    <View>
+      <Text>OrderStatusUpdate</Text>
+    </View>
+  )
+}
+
+export default OrderStatusUpdate
---
+++ components/ui/StatusIndicator.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const StatusIndicator = () => {
+  return (
+    <View>
+      <Text>StatusIndicator</Text>
+    </View>
+  )
+}
+
+export default StatusIndicator
