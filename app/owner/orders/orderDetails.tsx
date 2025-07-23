ac792c65e1cad5be0b2c0042191e0145262ff0d0
shashank-a21 <shashankanand705@gmail.com>
2025-07-23 11:41:41 +0530

    Added Boilerplate in components files

---
+++ app/owner/orders/orderDetails.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const orderDetails = () => {
+  return (
+    <View>
+      <Text>orderDetails</Text>
+    </View>
+  )
+}
+
+export default orderDetails
